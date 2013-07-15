cb8db3a7e44eff27c81a56b49490f9f451e46c0c
Andrew Smith <andrewchristophersmith@gmail.com>
2013-07-15 15:52:04 -0700

    Added a welcome controller with the index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Heimdaller::Application.routes.draw do
+  get "welcome/index"
   resources :characters
+  
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
