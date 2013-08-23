3e10d2109e9faac82100b2a2fb8c1f1c6075a2f7
W Eich <rw.eichelberger@gmail.com>
2013-08-23 08:21:18 -0700

    level model work

--- app/models/level.rb
+++ app/models/level.rb
@@ -6,4 +6,13 @@ class Level < ActiveRecord::Base
 	validates :fort_save, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 	validates :ref_save, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 	validates :will_save, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+	
+	CORE_CLASSES = %w{Barbarian Bard Cleric Druid Fighter Monk Paladin Ranger Rogue Sorcerer Wizard}
+	# Generate core class levels
+	# Barbarian_lvl1 Barbarian_lvl2 ... Barbarian_lvl20, etc.
+	CORE_CLASSES.each do |classs|
+		for i in 1..20
+			define_method "#{classs}_lvl"+i.to_s
+		end
+	end
 end
